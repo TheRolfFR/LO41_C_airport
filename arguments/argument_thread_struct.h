197112f68b4cdc50d33bee50952af25542d52e00
TheRolf <yann.lv@live.fr>
2020-05-31 16:55:04 +0200

    début remplissage controleurThread.h

--- arguments/argument_thread_struct.h
+++ arguments/argument_thread_struct.h
@@ -6,11 +6,13 @@
 #define LO41_PROJET_ARGUMENT_THREAD_STRUCT_H
 
 #include "../mutex/avions/mutex_avions_struct.h"
+#include "../piste/piste.h"
 
 /// arguments passés au thread : les mutex pour les avions, les mutex pour les pistes (inutile, voir mutex_pistes_struct.h pourquoi), la file de messages pour les avions
 typedef struct {
     mutex_avions_struct mutexAvions;
     int idFileMsgAvions;
+    piste mesPistes[2];
 } argument_thread_struct;
 
 #endif //LO41_PROJET_ARGUMENT_THREAD_STRUCT_H
--- controleur/controleurThread.h
+++ controleur/controleurThread.h
@@ -8,6 +8,9 @@
 #include "../avion/avion.h"
 #include "../constantes.h"
 #include "../piste/piste.h"
+#include "../arguments/argument_thread_struct.h"
+#include "../ipc/msgAvions.h"
+#include "../ipc/msgRecevoir.h"
 #include <pthread.h>
 
 /// Thread du controleur, doit être capapble de tout gérer
@@ -19,7 +22,9 @@ void *controleurThread(void* arg) {
      * - les mutex pour controler les avions (chaque avions individuellement mais aussi le mutex pour les avions en atterissage force)
      * - les mutex pour controler les pistes (inutile, voir mutex_pistes_struct.h pourquoi)
      * - l'id de la file de message pour récupérer les avions
+     * - les pistes
      */
+    argument_thread_struct *arguments = (argument_thread_struct *) arg;
 
     // on doit créer un tableau d'avions
     // attention: la liste d'avions est différente des deux listes d'attentes pour les pistesz
@@ -29,19 +34,21 @@ void *controleurThread(void* arg) {
      * on ne le vide pas car on s'en fiche totalement
      * cependant on va attendre d'avoir tous les avions avant de pouvoir continuer
      */
+    msg_avion msgAvion;
     for(int i = 0; i < NB_AVIONS; ++i) {
         // attente de messages IPC et ajout des avions à la liste
+        msgRecevoir(arguments->idFileMsgAvions, &msgAvion, MSG_AVIONS_TYPE);
 
         // ajout des nouveaux avions à la liste
+        tableauAvions[i] = msgAvion.a;
     }
 
     /*
      * On doit aussi avoir des références aux pistes et pour les controler
      */
     // ajout de pistes
-    piste mesPistes[2];
-    initialiserPiste(&mesPistes[0], true);
-    initialiserPiste(&mesPistes[1], false);
+    initialiserPiste(&arguments->mesPistes[0], true);
+    initialiserPiste(&arguments->mesPistes[1], false);
 
     // on doit creer nos deux listes d'attente
 
@@ -51,6 +58,7 @@ void *controleurThread(void* arg) {
     // "enfin" on peut lancer la boucle infinie qui lancera la gestion des pistes
     while(true) {
         // on attend l'évènement de libération d'une piste QUELCONQUE, bref d'une action d'avion quelconque
+        pthread_cond_wait(&arguments->mutexAvions.avionQuelconque, &arguments->mutexAvions.mutex);
 
         // on revoit la génération du planning
 
